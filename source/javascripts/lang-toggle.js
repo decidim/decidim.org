1e9c3f355a667c4ba0e174e08a4a47b3e8c5d24d
Javier usobiaga <javier@swwweet.com>
2017-06-19 21:28:22 +0200

    Optimised images. Lang toggle JS fix. Plain colors in Decidim logo

--- source/javascripts/lang-toggle.js
+++ source/javascripts/lang-toggle.js
@@ -4,7 +4,20 @@ function langToggle() {
     e.preventDefault();
     document.querySelector(".lang-wrapper").classList.toggle("is-lang-open");
   }
+
+  function langToggleClickOut(e) {
+		var targetElements,
+				targetTriggers,
+			i;
+
+		if (!e.target.closest(".lang-wrapper")) {
+			targetElements = document.querySelector(".lang-wrapper").classList.remove("is-lang-open");
+		}
+	}
+
   document.querySelector(".lang-trigger").addEventListener("click", toggleLangClass);
+  document.querySelector("body").addEventListener("click", langToggleClickOut);
+
 }
 
 langToggle();
