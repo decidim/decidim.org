a4edaf31abad254434912b6d649827fcbd3148eb
htmlboy <javier@swwweet.com>
2018-11-25 12:05:01 +0100

    JS console error fixed

--- source/javascripts/play-video.js
+++ source/javascripts/play-video.js
@@ -1,33 +1,37 @@
-function playVideo(){
-  var videoWrapper = document.querySelector("[data-video-wrapper]"),
-      videoIframe = document.querySelector("[data-video-wrapper] iframe"),
-      videoSrc = videoIframe.dataset.src;
+function playVideo() {
+  var videoWrapper = document.querySelector('[data-video-wrapper]')
 
-  if(!videoWrapper){
-    return;
+  if (!videoWrapper) {
+    return
+  }
+  var videoIframe = document.querySelector('[data-video-wrapper] iframe'),
+    videoSrc = videoIframe.dataset.src
+
+  function showVideo(e) {
+    e.preventDefault()
+    videoWrapper.classList.add('is-video-active')
+    videoIframe.src = videoSrc
+    document.body.classList.add('is-video-playing')
+    videoWrapper.tabIndex = -1
+    videoWrapper.style.outline = 0
+    videoWrapper.focus()
   }
 
-  function showVideo(e){
-    e.preventDefault();
-    videoWrapper.classList.add("is-video-active");
-    videoIframe.src=videoSrc;
-    document.body.classList.add("is-video-playing");
-    videoWrapper.tabIndex = -1;
-    videoWrapper.style.outline = 0;
-    videoWrapper.focus();
+  function hideVideo(e) {
+    e.preventDefault()
+    videoWrapper.classList.remove('is-video-active')
+    videoIframe.src = ''
+    document.body.classList.remove('is-video-playing')
   }
-
-  function hideVideo(e){
-    e.preventDefault();
-    videoWrapper.classList.remove("is-video-active");
-    videoIframe.src="";
-    document.body.classList.remove("is-video-playing");
-  }
-  document.querySelector("[data-playvideo-button]").addEventListener("click", showVideo);
-  document.querySelector("[data-closevideo]").addEventListener("click", hideVideo);
-  document.querySelector("[data-video-wrapper]").addEventListener("click", hideVideo);
+  document
+    .querySelector('[data-playvideo-button]')
+    .addEventListener('click', showVideo)
+  document
+    .querySelector('[data-closevideo]')
+    .addEventListener('click', hideVideo)
+  document
+    .querySelector('[data-video-wrapper]')
+    .addEventListener('click', hideVideo)
 }
 
-
-
-playVideo();
+playVideo()
