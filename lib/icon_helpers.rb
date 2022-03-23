61d6abf0688d53cff06cfe8380e7a24b8627f5ee
Hugoren Martinako <aumpfbahn@gmail.com>
2022-03-23 10:49:56 +0100

    desktop header

--- lib/icon_helpers.rb
+++ lib/icon_helpers.rb
@@ -1,6 +1,6 @@
 module IconHelpers
-  def icon(id)
-    content_tag(:svg, content_tag(:use, "", { "xlink:href" => "/images/remixicon.symbol.svg#" + id.to_s }), { "width" => "1em", "height" => "1em" })
+  def icon(id, args = {})
+    content_tag(:svg, content_tag(:use, "", { "xlink:href" => "/images/remixicon.symbol.svg#" + id.to_s }), { "width" => "1em", "height" => "1em", "fill" => "currentColor" }.merge(args))
   end
 
   def list_icons
