9026bd6b606f89d890de90e8707073d932e85cbc
Hugoren Martinako <aumpfbahn@gmail.com>
2022-04-25 18:27:12 +0200

    animation mobile infinite loop

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,39 +15,47 @@ module.exports = {
       },
       white: "#fff",
       black: "#000",
-      transparent: "transparent"
+      transparent: "transparent",
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "1.5rem",
         md: "4rem",
-        xl: "8rem"
+        xl: "8rem",
       },
     },
     fontFamily: {
-      "sans": ["Barlow", "system-ui", "sans-serif"]
+      sans: ["Barlow", "system-ui", "sans-serif"],
     },
     extend: {
       keyframes: {
         fadeIn: {
-          to: { opacity: "100%", transform: "rotate(180deg)" },
+          to: { opacity: 1, transform: "rotate(180deg)" },
         },
         fadeOut: {
-          to: { opacity: "0%", transform: "rotate(180deg)" },
+          to: { opacity: 0, transform: "rotate(180deg)" },
         },
-        slide: {
-          to: { transform: "translate3d(var(--translate-slide), 0, 0)" },
-        }
+        slideX: {
+          to: { transform: "translateX(var(--translate-x))" },
+        },
+        slideY: {
+          // percetages will vary based on the amount of items
+          "0%": { opacity: 0, transform: "translate3d(0, calc(100% - (100% * var(--translate-ix))), 0)" },
+          "2%": { opacity: 1, transform: "translate3d(0, calc(-100% * var(--translate-ix)), 0)" },
+          "8%": { opacity: 1, transform: "translate3d(0, calc(-100% * var(--translate-ix)), 0)" },
+          "10%": { opacity: 0, transform: "translate3d(0, calc(-100% - (100% * var(--translate-ix))), 0)" },
+          "100%": { opacity: 0, transform: "translate3d(0, calc(-100% - (100% * var(--translate-ix))), 0)" },
+        },
       },
       animation: {
         "fade-in": "fadeIn 0.3s ease-in-out forwards",
         "fade-out": "fadeOut 0.3s ease-in-out forwards",
-        "slide": "slide 10s linear infinite"
-      }
-    }
+        "slide-x": "slideX 15s linear infinite",
+        // duration will vary based on the amount of items
+        "slide-y": "slideY 18s cubic-bezier(.19,1,.22,1) calc(2s * var(--translate-ix)) infinite",
+      },
+    },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
