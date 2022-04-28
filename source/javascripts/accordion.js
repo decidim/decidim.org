a5b2afc6274afdb3fa6eec026c198d5d66bda3d4
Hugoren Martinako <aumpfbahn@gmail.com>
2022-04-28 14:12:59 +0200

    refactoring accordion for semantic html

--- source/javascripts/accordion.js
+++ source/javascripts/accordion.js
@@ -1,39 +1,30 @@
 /**
  * Use [data-accordion] tag in the parent node of every element
- * Use [data-accordion-id=UNIQUE_ID] tag in the items, setting an unique id
- * The first child's item will be the trigger, and the last-child the toggle content
  *
  * @example
  * <div data-accordion>
- *   <div data-accordion-id="value1">...</div>
- *   <div data-accordion-id="value2">...</div>
- *   <div data-accordion-id="value3">...</div>
+ *   <div>...</div>
+ *   <div>...</div>
+ *   <div>...</div>
  * </div>
  */
-function getAccordionId(node) {
-  const { dataset: { accordionId } = {}} = node || {}
-  return accordionId
-}
-
-function handleAccordionClick({ target }) {
-  const accordion = target.closest("[data-accordion]")
-  const item = target.closest("[data-accordion-id]")
-
-  if (item) {
-    const ACTIVE_CSS_CLASS = "is-selected";
-    const itemId = getAccordionId(item);
-
-    [...accordion.children].forEach((node) =>
-      getAccordionId(node) === itemId && !node.classList.contains(ACTIVE_CSS_CLASS)
-        ? node.classList.add(ACTIVE_CSS_CLASS)
-        : node.classList.remove(ACTIVE_CSS_CLASS)
-    );
-  }
-}
 
 function accordion() {
-  const selectors = document.querySelectorAll("[data-accordion]")
-  selectors.forEach(container => container.addEventListener("click", handleAccordionClick))
+  const selectors = document.querySelectorAll("[data-accordion]");
+  selectors.forEach((container) => {
+    const details = Array.from(container.querySelectorAll("details"));
+
+    if (details.length) {
+      details.forEach((detail) =>
+        detail.addEventListener("click", (e) => {
+          const active = details.find((d) => d.open);
+          if (!e.currentTarget.open && active) {
+            active.open = false;
+          }
+        })
+      );
+    }
+  });
 }
 
 accordion()
