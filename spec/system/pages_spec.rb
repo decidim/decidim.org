183afa6112260c4046857b7f290d34bd4b42f613
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-10-10 14:27:56 +0200

    Fix links on pages with i18n

---
+++ spec/system/pages_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "pages", type: :feature do
+  before do
+    visit "/trademark-policy"
+  end
+
+  it "has the correct title header" do
+    expect(page).to have_selector "h1"
+    within "h1" do
+      expect(page).to have_content(/Trademark policy/i)
+    end
+  end
+
+  it "has the correct links" do
+    expect(page).to have_selector "main"
+
+    within "main" do
+      expect(page).to have_link "Ruby on Rails", href: "https://rubyonrails.org/trademarks"
+      expect(page).to have_link "Wordpress Foundation", href: "https://wordpressfoundation.org/trademark-policy/"
+    end
+  end
+end
