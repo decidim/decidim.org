16d37be8328ba59d94d06abf47808ff908c2880b
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-04-16 16:08:50 +0200

    Condition added to event listener for touch tabs on mobile

--- source/javascripts/tabs.js
+++ source/javascripts/tabs.js
@@ -58,7 +58,11 @@ function tabs() {
   const selectors = document.querySelectorAll("[data-tabs]")
   selectors.forEach(container => {
     container.addEventListener("click", (event) => handleTabClick({ event, type: container.dataset.tabs }));
-    container.addEventListener("pointerover", (event) => handleTabClick({ event, type: container.dataset.tabs }));
+    container.addEventListener("pointerover", (event) => {
+      if (event.pointerType !== "touch") {
+        handleTabClick({ event, type: container.dataset.tabs });
+      }
+    });
     container.addEventListener("keydown", (event) => event.key === "Enter" && handleTabClick({ event, type: container.dataset.tabs }));
 
     let startX;
