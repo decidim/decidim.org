e93497ce749f9b27f01bc3c6ca94ff3816c04ea4
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-04-23 13:41:39 +0200

    Added a documentation spec to test for page link and the page loads

---
+++ spec/system/documentation_spec.rb
@@ -0,0 +1,20 @@
+# frozen_literal: true
+
+require "spec_helper"
+RSpec.describe "documentation", type: :feature do
+  before do
+    visit "/documentation"
+  end
+
+  it "has the correct title header" do
+    expect(page).to have_text "Documentation"
+  end
+
+  it "has a link to the API documentation page" do
+    expect(page).to have_link "Documentation", href: "https://docs.decidim.org"
+  end
+
+  it "loads without 500 error" do
+    expect(page.status_code).to eq(200)
+  end
+end
