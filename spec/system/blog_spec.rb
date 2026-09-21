6c7dc8d5770851bf54579f7829180b804105d4ab
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-09-21 15:27:01 +0200

    Fix blog_specs

--- spec/system/blog_spec.rb
+++ spec/system/blog_spec.rb
@@ -14,43 +14,43 @@ RSpec.describe "blog", type: :feature do
   end
 
   it "filters cards when a type filter is selected", :js do
-    find("[data-cs-filter-toggle]").click
+    find("[data-filter-toggle]").click
     check "Product"
 
-    expect(page).to have_css("[data-cs-card][data-type='Product']", count: 1)
+    expect(page).to have_css("[data-filter-card][data-type='Product']", count: 1)
   end
 
   it "removes a filter when the chip close button is clicked", :js do
-    find("[data-cs-filter-toggle]").click
+    find("[data-filter-toggle]").click
     check "Product"
-    find("[data-cs-active-chips] button[aria-label='Remove Product']").click
+    find("[data-filter-chips] button[aria-label='Remove Product']").click
 
-    expect(page).to have_no_css("[data-cs-active-chips]", text: "Product")
+    expect(page).to have_no_css("[data-filter-chips]", text: "Product")
   end
 
   it "filters cards when typing in the search box", :js do
     fill_in "Search...", with: "Mozambique"
 
-    expect(page).to have_css("[data-cs-card]", count: 1, text: "Mozambique")
+    expect(page).to have_css("[data-filter-card]", count: 1, text: "Mozambique")
   end
 
   it "provides no matchable cards when the search term does not match any blog post", :js do
     fill_in "Search...", with: "no results here today!"
 
-    expect(page).to have_css("[data-cs-card]", count: 0)
+    expect(page).to have_css("[data-filter-card]", count: 0)
   end
 
   it "provides a message when the search term does not match any blog post", :js do
     fill_in "Search...", with: "no results here today!"
 
-    expect(page).to have_text("No case studies found matching your filters.")
+    expect(page).to have_text("No results found matching your filters.")
   end
 
   it "allows the clearing of filters within the filter panel", :js do
-    find("[data-cs-filter-toggle]").click
+    find("[data-filter-toggle]").click
     check "Announcements"
 
-    find("[data-cs-clear-filters]").click
+    find("[data-filter-clear]").click
     expect(page).to have_unchecked_field("Announcements")
   end
 end
