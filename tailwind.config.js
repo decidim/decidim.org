c270f78b9d59b03a573f235dd5aee3df64c85cea
Tom Greenwood <tom.t.greenwood@gmail.com>
2026-09-23 10:28:50 +0200

    Fix trailing space commas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,50 @@
 module.exports = {
   content: ["./source/**/*.{html,js,erb}"],
   safelist: [
-    'prose-li:!mt-0',
-    'prose-ul:!mt-0',
-    'prose-li:!mb-0',
-    'prose-p:!mt-0',
-    'prose-li:marker:text-black',
+    "prose-li:!mt-0",
+    "prose-ul:!mt-0",
+    "prose-li:!mb-0",
+    "prose-p:!mt-0",
+    "prose-li:marker:text-black"
   ],
   theme: {
     colors: {
       red: {
         100: "#fff0f0",
         500: "#ff3333",
-        900: "#c20a0a",
+        900: "#c20a0a"
       },
       gray: {
-        50:  "#fafafa",
+        50: "#fafafa",
         100: "#f5f5f5",
         300: "#d4d4d4",
-        500: "#656565",
+        500: "#656565"
       },
       white: "#fff",
       black: "#000",
-      transparent: "transparent",
+      transparent: "transparent"
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "1.5rem",
         md: "4rem",
-        xl: "10.625rem",
-      },
+        xl: "10.625rem"
+      }
     },
     fontFamily: {
-      sans: ["Barlow", "system-ui", "sans-serif"],
+      sans: ["Barlow", "system-ui", "sans-serif"]
     },
     extend: {
       keyframes: {
         fadeIn: {
-          to: { opacity: 1, transform: "rotate(180deg)" },
+          to: { opacity: 1, transform: "rotate(180deg)" }
         },
         fadeOut: {
-          to: { opacity: 0, transform: "rotate(180deg)" },
+          to: { opacity: 0, transform: "rotate(180deg)" }
         },
         slideX: {
-          to: { transform: "translateX(var(--translate-x))" },
+          to: { transform: "translateX(var(--translate-x))" }
         },
         slideY: {
           // percetages will vary based on the amount of items
@@ -52,7 +52,7 @@ module.exports = {
           "2%": { opacity: 1, transform: "translate3d(0, calc(-100% * var(--translate-ix)), 0)" },
           "8%": { opacity: 1, transform: "translate3d(0, calc(-100% * var(--translate-ix)), 0)" },
           "10%": { opacity: 0, transform: "translate3d(0, calc(-100% - (100% * var(--translate-ix))), 0)" },
-          "100%": { opacity: 0, transform: "translate3d(0, calc(-100% - (100% * var(--translate-ix))), 0)" },
+          "100%": { opacity: 0, transform: "translate3d(0, calc(-100% - (100% * var(--translate-ix))), 0)" }
         },
       },
       animation: {
@@ -60,9 +60,9 @@ module.exports = {
         "fade-out": "fadeOut 0.3s ease-in-out forwards",
         "slide-x": "slideX 15s linear infinite",
         // duration will vary based on the amount of items
-        "slide-y": "slideY 18s cubic-bezier(.19,1,.22,1) calc(2s * var(--translate-ix)) infinite",
-      },
-    },
+        "slide-y": "slideY 18s cubic-bezier(.19,1,.22,1) calc(2s * var(--translate-ix)) infinite"
+      }
+    }
   },
   plugins: [require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp")
